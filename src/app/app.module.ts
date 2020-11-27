32bd0204019797a12a971ff1c66a92f996f74fb8
elise young <eliseyoung_@hotmail.com>
2020-11-27 09:15:46 +0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule }from '@angular/common/http';
+import { StartComponent } from './components/start.component';
+import { LoginComponent } from './components/login.component'
+
+const ROUTES: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'searchList', component: SearchListComponent},
+  {path: 'search', component: SearchComponent},
+  {path: 'search/:type/:q', component: ResultComponent},
+  {path: '**', redirectTo: "/", pathMatch:'full'},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES),
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
